90eeb9f41e3c9238cf80ab5f95aea2638b93922e
MiguelG5 <mikeyjaime99@gmail.com>
2024-06-18 09:28:24 -0600

    Actualizacion de tabla paquetes

--- src/controllers/paquetes.controllers.js
+++ src/controllers/paquetes.controllers.js
@@ -16,17 +16,17 @@ paquetesCtrl.getPaquetes = async (req, res) => {
 // Crear un nuevo paquete
 paquetesCtrl.createPaquete = async (req, res) => {
   try {
-    const { nombre_paquete, descripcion, costo, duracion } = req.body;
+    const { nombre_paquete, descripcion, costo, duracion, numero_colaboradores } = req.body;
 
-    if (!nombre_paquete || !costo || !duracion) {
+    if (!nombre_paquete || !costo || !duracion || numero_colaboradores === undefined) {
       return res.status(400).json({
-        error: "nombre_paquete, costo y duracion son campos requeridos.",
+        error: "nombre_paquete, costo, duracion y numero_colaboradores son campos requeridos.",
       });
     }
 
     const result = await pool.query(
-      "INSERT INTO paquetes (nombre_paquete, descripcion, costo, duracion) VALUES ($1, $2, $3, $4) RETURNING *",
-      [nombre_paquete, descripcion, costo, duracion]
+      "INSERT INTO paquetes (nombre_paquete, descripcion, costo, duracion, numero_colaboradores) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [nombre_paquete, descripcion, costo, duracion, numero_colaboradores]
     );
 
     res.status(201).json(result.rows[0]);
@@ -65,7 +65,7 @@ paquetesCtrl.getPaquete = async (req, res) => {
 paquetesCtrl.editPaquete = async (req, res) => {
   try {
     const { id } = req.params;
-    const { nombre_paquete, descripcion, costo, duracion } = req.body;
+    const { nombre_paquete, descripcion, costo, duracion, numero_colaboradores } = req.body;
 
     if (!id) {
       return res.status(400).send("ID del paquete es requerido.");
@@ -92,6 +92,11 @@ paquetesCtrl.editPaquete = async (req, res) => {
     if (duracion) {
       updateFields.push(`duracion = $${values.length + 1}`);
       values.push(duracion);
+    }
+
+    if (numero_colaboradores !== undefined) {
+      updateFields.push(`numero_colaboradores = $${values.length + 1}`);
+      values.push(numero_colaboradores);
     }
 
     const updateQuery = `UPDATE paquetes SET ${updateFields.join(
